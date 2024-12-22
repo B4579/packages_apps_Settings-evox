PixelExperience/packages_apps_Settings

Settings: Introduce Smart Pause [2/2]
* Animation by Sahul Krishan
* Adapt to Android 11 by @mickaelmendes50

[Hernán Castañón] Adaptive Playback: allow choosing no timeout.

Also includes:
commit c2a0ae029c19a5895a0663c22a4294c267479fe4 (HEAD -> thirteen-plus)
Author: Mesquita <mesquita@aospa.co>
Date:   Sun Feb 20 12:20:05 2022 -0600

    AdaptivePlayback: Rename it to 'Smart Pause' to keep a better fluency

     * As we already have an 'Adaptive Sound' in sound settings

    Change-Id: Iaca4b7d65fe490ee6462ba49907316930deaeaa5
    Signed-off-by: Mesquita <mesquita@aospa.co>

commit 2ca3ccb608fc0bf7f45a4cf714a7e4db77118008
Author: Mesquita <mesquita@aospa.co>
Date:   Sun Feb 13 20:21:54 2022 +0900

    Settings: Move "Adaptive Playback" option to Sound Settings.

     * Our gestures category looks bloated with features.
       Move this to Sound Settings where it feels better and more homely.

    Change-Id: I28fd022a4998141435a1e89a027b7a1fe6f75537
    Signed-off-by: Mesquita <mesquita@aospa.co>

commit c5a064bb1d5823524d82ee41e97eee552f906cc2
Author: Mesquita <mesquita@aospa.co>
Date:   Sun Feb 20 09:29:52 2022 -0600

    AdaptivePlayback: Add a intro text to the fragment

     * Let's help users to understand what the feature does
       by adding a little introduction text.

    Change-Id: Ie9ef15d7f60ab2197c30c79434c9fb876c498cd5
    Co-authored-by: AshutoshSundresh <ashutoshsundresh@gmail.com>
    Signed-off-by: Mesquita <mesquita@aospa.co>

Co-authored-by: Hernán Castañón Álvarez <herna@paranoidandroid.co>
Change-Id: Ia9c04150a073adc3aec258078b6da0a3e726048b

src/com/android/settings/sound/AdaptivePlaybackSwitchPreferenceController.java
@@ -1,0 +1,115 @@
+/*
+ * Copyright (C) 2020-2022 Paranoid Android
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.sound;
+
+import android.content.Context;
+import android.database.ContentObserver;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.widget.Switch;
+
+import androidx.preference.PreferenceScreen;
+
+import com.android.settings.widget.SettingsMainSwitchPreferenceController;
+import com.android.settingslib.core.lifecycle.LifecycleObserver;
+import com.android.settingslib.core.lifecycle.events.OnStart;
+import com.android.settingslib.core.lifecycle.events.OnStop;
+import com.android.settingslib.widget.MainSwitchPreference;
+
+public class AdaptivePlaybackSwitchPreferenceController extends
+        SettingsMainSwitchPreferenceController implements LifecycleObserver, OnStart, OnStop {
+
+    private MainSwitchPreference mPreference;
+    private final SettingsObserver mSettingsObserver;
+
+    public AdaptivePlaybackSwitchPreferenceController(Context context, String preferenceKey) {
+        super(context, preferenceKey);
+        mSettingsObserver = new SettingsObserver(new Handler(Looper.getMainLooper()));
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return AVAILABLE;
+    }
+
+    @Override
+    public boolean isChecked() {
+        return Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.ADAPTIVE_PLAYBACK_ENABLED, 0, UserHandle.USER_CURRENT) != 0;
+    }
+
+    @Override
+    public boolean setChecked(boolean isChecked) {
+        Settings.System.putIntForUser(mContext.getContentResolver(),
+                Settings.System.ADAPTIVE_PLAYBACK_ENABLED, isChecked ? 1 : 0,
+                UserHandle.USER_CURRENT);
+        return true;
+    }
+
+    @Override
+    public void displayPreference(PreferenceScreen screen) {
+        super.displayPreference(screen);
+        mPreference = screen.findPreference(getPreferenceKey());
+    }
+
+    @Override
+    public void onStart() {
+        mSettingsObserver.observe();
+    }
+
+    @Override
+    public void onStop() {
+        mContext.getContentResolver().unregisterContentObserver(mSettingsObserver);
+    }
+
+    @Override
+    public void onSwitchChanged(Switch switchView, boolean isChecked) {
+        Settings.System.putIntForUser(mContext.getContentResolver(),
+                Settings.System.ADAPTIVE_PLAYBACK_ENABLED, isChecked ? 1 : 0,
+                UserHandle.USER_CURRENT);
+    }
+
+    @Override
+    public int getSliceHighlightMenuRes() {
+        return NO_RES;
+    }
+
+    private final class SettingsObserver extends ContentObserver {
+        private final Uri ADAPTIVE_PLAYBACK = Settings.System.getUriFor(
+                Settings.System.ADAPTIVE_PLAYBACK_ENABLED);
+
+        public SettingsObserver(Handler handler) {
+            super(handler);
+        }
+
+        public void observe() {
+            mContext.getContentResolver().registerContentObserver(ADAPTIVE_PLAYBACK, false, this,
+                    UserHandle.USER_ALL);
+        }
+
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
+            super.onChange(selfChange, uri);
+            if (ADAPTIVE_PLAYBACK.equals(uri)) {
+                mPreference.setChecked(isChecked());
+            }
+        }
+    }
+}

B4579/packages_apps_Settings-evox

src/com/android/settings/sound/AdaptivePlaybackSwitchPreferenceController.java
@@ -1,0 +1,116 @@
+/*
+ * Copyright (C) 2020-2022 Paranoid Android
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.sound;
+
+import android.content.Context;
+import android.database.ContentObserver;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+
+import androidx.preference.PreferenceScreen;
+
+import com.android.settings.widget.SettingsMainSwitchPreferenceController;
+import com.android.settingslib.core.lifecycle.LifecycleObserver;
+import com.android.settingslib.core.lifecycle.events.OnStart;
+import com.android.settingslib.core.lifecycle.events.OnStop;
+import com.android.settingslib.widget.MainSwitchPreference;
+
+public class AdaptivePlaybackSwitchPreferenceController extends
+        SettingsMainSwitchPreferenceController implements LifecycleObserver, OnStart, OnStop {
+
+    private MainSwitchPreference mPreference;
+    private final SettingsObserver mSettingsObserver;
+
+    public AdaptivePlaybackSwitchPreferenceController(Context context, String preferenceKey) {
+        super(context, preferenceKey);
+        mSettingsObserver = new SettingsObserver(new Handler(Looper.getMainLooper()));
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return AVAILABLE;
+    }
+
+    @Override
+    public boolean isChecked() {
+        return Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.ADAPTIVE_PLAYBACK_ENABLED, 0, UserHandle.USER_CURRENT) != 0;
+    }
+
+    @Override
+    public boolean setChecked(boolean isChecked) {
+        Settings.System.putIntForUser(mContext.getContentResolver(),
+                Settings.System.ADAPTIVE_PLAYBACK_ENABLED, isChecked ? 1 : 0,
+                UserHandle.USER_CURRENT);
+        return true;
+    }
+
+    @Override
+    public void displayPreference(PreferenceScreen screen) {
+        super.displayPreference(screen);
+        mPreference = screen.findPreference(getPreferenceKey());
+    }
+
+    @Override
+    public void onStart() {
+        mSettingsObserver.observe();
+    }
+
+    @Override
+    public void onStop() {
+        mContext.getContentResolver().unregisterContentObserver(mSettingsObserver);
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        Settings.System.putIntForUser(mContext.getContentResolver(),
+                Settings.System.ADAPTIVE_PLAYBACK_ENABLED, isChecked ? 1 : 0,
+                UserHandle.USER_CURRENT);
+    }
+
+    @Override
+    public int getSliceHighlightMenuRes() {
+        return NO_RES;
+    }
+
+    private final class SettingsObserver extends ContentObserver {
+        private final Uri ADAPTIVE_PLAYBACK = Settings.System.getUriFor(
+                Settings.System.ADAPTIVE_PLAYBACK_ENABLED);
+
+        public SettingsObserver(Handler handler) {
+            super(handler);
+        }
+
+        public void observe() {
+            mContext.getContentResolver().registerContentObserver(ADAPTIVE_PLAYBACK, false, this,
+                    UserHandle.USER_ALL);
+        }
+
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
+            super.onChange(selfChange, uri);
+            if (ADAPTIVE_PLAYBACK.equals(uri)) {
+                mPreference.setChecked(isChecked());
+            }
+        }
+    }
+}